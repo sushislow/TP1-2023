a8cba7a7e9ad136329bc5dc0fe35b0213e65f6f3
sushislow <eduardosf2307@gmail.com>
2023-09-27 11:45:24 -0300

    Aula-27/09

---
+++ ejs+express.js
@@ -0,0 +1,18 @@
+const port = 8080;
+
+//Express
+const express = require("express");
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get("/index", (request, response) => {
+response.render("index"); //views/index.ejs
+});
+
+app.use((request, response, next) => {
+response.status(404).send("<h1>Página não encontrada.</h1>");
+})
+
+app.listen(port, () => {
+console.log(`Servidor funcionando na porta: ${port}`);
+});
