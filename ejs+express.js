08ae74e2cd21c88df4688261c5cf2d5d1ea1eb02
sushislow <eduardosf2307@gmail.com>
2023-09-27 12:35:57 -0300

    Aula-27/09

--- ejs+express.js
+++ ejs+express.js
@@ -8,7 +8,9 @@ app.set('view engine', 'ejs');
 app.get("/index", (request, response) => {
 response.render("index"); //views/index.ejs
 });
-
+app.get("/ola", (request, response) => {
+    response.send (`Olá, ${request.query.nome} ${request.query.sobrenome}`)
+})
 app.use((request, response, next) => {
 response.status(404).send("<h1>Página não encontrada.</h1>");
 })
