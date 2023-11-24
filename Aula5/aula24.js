e7c5a46a11302755a5a2446f1bbafd9ec338bf6e
Eduardo <eduferreira2307@gmail.com>
2023-11-23 23:06:34 -0300

    nao funciona

--- Aula5/aula24.js
+++ Aula5/aula24.js
@@ -2,6 +2,8 @@ const port = 5950;
 const ip = Object.values(require('os').networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat(i.family==='IPv4' && !i.internal && i.address || []), [])), [])[0]
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({ extended: true }))
+const fs = require("fs");
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
@@ -11,8 +13,21 @@ app.get("/", (req, res) => {
 app.get("/obj", (req, res) => {
     res.render('obj',{title:'Denúncia Segura'});
 })
-app.get("/tec", (req, res) => {
-    res.render('tec',{title:'Denúncia Segura'});
+app.get("/email", (req, res) => {
+    res.render('email',{title:'Denúncia Segura'});
+})
+app.get("/", (req, res) => {
+    res.render("email"); 
+    resultado = ''
+});
+app.post("/salvar", (req, res) => {
+    let emailForm = req.body.email
+    let cadastro = { email: emailForm }
+    //console.log(cadastro);
+    //console.log('\n'+JSON.stringify(cadastro+','))
+    fs.appendFileSync('email.json', `\n  ${JSON.stringify(cadastro)}`)
+    resultado = `Olá ${cadastro.email}`
+    res.render('email', { resultado });
 })
 
 app.listen(port, () => {
