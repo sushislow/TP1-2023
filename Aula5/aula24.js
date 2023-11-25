2949432352121a15ae2e13b7a897ea4c668005f5
Eduardo <eduferreira2307@gmail.com>
2023-11-25 00:50:44 -0300

    O server :((((

--- Aula5/aula24.js
+++ Aula5/aula24.js
@@ -1,33 +1,38 @@
 const port = 5950;
-const ip = Object.values(require('os').networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat(i.family==='IPv4' && !i.internal && i.address || []), [])), [])[0]
-const express = require("express");
+//const ip = Object.values(require('os').networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat(i.family==='IPv4' && !i.internal && i.address || []), [])), [])[0]
+const express = require('express');
 const app = express();
-app.use(express.urlencoded({ extended: true }))
-const fs = require("fs");
-app.set('view engine', 'ejs');
+const fs = require('fs');
+app.set('view engine', "ejs");
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
     res.render('index', {title:'Denúncia Segura'});
 })
 app.get("/obj", (req, res) => {
-    res.render('obj',{title:'Denúncia Segura'});
+    res.render('obj', {title:'Denúncia Segura'});
 })
 app.get("/email", (req, res) => {
-    res.render('email',{title:'Denúncia Segura'});
+    res.render('email', {title:'Denúncia Segura'});
 })
-app.get("/", (req, res) => {
+app.get("/email", (req, res) => {
     res.render("email"); 
+    res.render('email', {title:'Denúncia Segura'});
     resultado = ''
-});
-app.post("/salvar", (req, res) => {
-    let emailForm = req.body.email
-    let cadastro = { email: emailForm }
+    res.render('email', { resultado });
+    
+})
+app.post('/salvar', (req, res) => {
+    let emailNoForm = req.body.email
+    let cadastro = { email: emailNoForm }
     //console.log(cadastro);
     //console.log('\n'+JSON.stringify(cadastro+','))
-    fs.appendFileSync('email.json', `\n  ${JSON.stringify(cadastro)}`)
-    resultado = `Olá ${cadastro.email}`
+    fs.appendFileSync('Denuncias.json', `\n  ${JSON.stringify(cadastro)}`)
+    resultado = `Olá ${emailNoForm}`
+    res.render('email', {title:'Denúncia Segura'});
     res.render('email', { resultado });
+    
 })
 
 app.listen(port, () => {
