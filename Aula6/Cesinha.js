295c613ca38427e4f2c7b9906a31f184d1f997cc
DanielzGon <danielgoncalvescarvalho12@gmail.com>
2023-11-29 12:11:17 -0300

    29-11-23

--- Aula6/Cesinha.js
+++ Aula6/Cesinha.js
@@ -6,22 +6,33 @@ const app = express();
 app.use(express.urlencoded({ extended: true }))
 const fs = require("fs");
 app.set('view engine', 'ejs');
+ 
+let vetornomes = []
+if (fs.existsSync('nome.json')){
+    const dados = fs.readFileSync('nome.json', 'utf-8')
+    console.log(dados);
+    vetornomes = JSON.parse(dados)
+}
 
 app.get("/", (request, response) => {
     response.render("result"); //views/teste4.ejs
     resultado = ''
 });
-app.post("/teste4", (request, response) => {
-    let nomeNoForm = request.body.nome
+app.post("/teste4", (req, res) => {
+    let nomeNoForm = req.body.nome
     let cadastro = { nome: nomeNoForm }
-    //console.log(cadastro);
-    //console.log('\n'+JSON.stringify(cadastro+','))
-    fs.appendFileSync('nome.json', `\n  ${JSON.stringify(cadastro)}`)
+    console.log(cadastro);
+    console.log('\n'+JSON.stringify(cadastro+','))
+    vetornomes.push(cadastro)
+    fs.writeFileSync('nome.json', `\n  ${JSON.stringify(vetornomes)}`)
     resultado = `Olá ${cadastro.nome}`
-    response.render('result', { resultado });
+    res.render('result', { resultado });
 })
-app.use((request, response, next) => {
-    response.status(404).send("<h1>Página não encontrada.</h1>");
+app.get('/nomes'), (req,res) => {
+    res.render('nomes', {vetornomes})
+}
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Página não encontrada.</h1>");
 })
 
 app.listen(port, () => {
